abe24beebba3ca444b65c3cb108b9d8c2250ca60
ShadowBudgie <shadowbudgie@gmail.com>
2024-08-30 12:19:32 +0200

    Added Config Options for Fade Speed and Song switch Speed

--- src/main/java/circuitlord/reactivemusic/config/ModConfig.java
+++ src/main/java/circuitlord/reactivemusic/config/ModConfig.java
@@ -8,12 +8,14 @@ import com.google.gson.GsonBuilder;
 import com.terraformersmc.modmenu.ModMenu;
 import dev.isxander.yacl3.api.*;
 import dev.isxander.yacl3.api.controller.DropdownStringControllerBuilder;
+import dev.isxander.yacl3.api.controller.IntegerSliderControllerBuilder;
 import dev.isxander.yacl3.api.controller.EnumControllerBuilder;
 import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import dev.isxander.yacl3.gui.controllers.BooleanController;
+import dev.isxander.yacl3.gui.controllers.slider.IntegerSliderController;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -58,6 +60,13 @@ public class ModConfig {
     @SerialEntry
     public String loadedUserSongpack = "";
 
+    @SerialEntry
+    public Integer standardFadeout = 0;
+
+    @SerialEntry
+    public Integer waitforSwitch = 150;
+
+
 
 
     public static Screen createScreen(Screen parent) {
@@ -81,7 +90,6 @@ public class ModConfig {
                                     .text(Text.literal("The included songpack with Reactive Music."))
                                     .build()
                     )
-
                     .available(!arIsLoaded)
                     .text(Text.literal(arIsLoaded ? "Loaded" : "Load"))
                     .action((yaclScreen, buttonOption) -> {
@@ -89,25 +97,19 @@ public class ModConfig {
                         ReactiveMusic.refreshSongpack();
                         MinecraftClient.getInstance().setScreen(ModConfig.createScreen(parent));
                     })
-
                     .build());
 
 
             for (var songpackZip : SongLoader.availableSongpacks) {
 
                 boolean isLoaded = Objects.equals(SongLoader.activeSongpack.name, songpackZip.config.name);
-
-
                 songpacksBuilder.option(ButtonOption.createBuilder()
                         .name(Text.literal(songpackZip.config.name))
                         .description(
                                 OptionDescription.createBuilder()
                                         .text(Text.literal("TODO"))
-                                        .build()
-                        )
-
+                                        .build())
                         .available(!isLoaded)
-
                         .text(Text.literal(isLoaded ? "Loaded" : "Load"))
 
 
@@ -116,63 +118,49 @@ public class ModConfig {
                             ReactiveMusic.refreshSongpack();
                             MinecraftClient.getInstance().setScreen(ModConfig.createScreen(parent));
                         })
-
-
-
                         .build());
-
-
             }
-
-
             builder.category(songpacksBuilder.build());
 
-
-
-
-
-            builder
-                    .title(Text.literal("Reactive Music"))
-
-
-                    .category(ConfigCategory.createBuilder()
-                            .name(Text.literal("General"))
-
-                            .option(Option.<MusicDelayLength>createBuilder()
-                                    .name(Text.literal("Music Delay Length"))
-                                    .binding(defaults.musicDelayLength, () -> config.musicDelayLength, newVal -> config.musicDelayLength = newVal )
-                                    .controller(opt -> EnumControllerBuilder.create(opt).enumClass(MusicDelayLength.class))
-
-                                    .build())
-
-                            .build())
-
-
-                    .category(ConfigCategory.createBuilder()
-                            .name(Text.literal("Debug"))
-                            .tooltip(Text.literal("Any debug tools useful for songpack creators or developers"))
-
-
-                            .option(Option.<Boolean>createBuilder()
+            builder.title(Text.literal("Reactive Music"))
+                   //Category / Tab "General"
+                   .category(ConfigCategory.createBuilder()
+                           .name(Text.literal("General"))
+                           .option(Option.<MusicDelayLength>createBuilder()
+                                   .name(Text.literal("Music Delay Length"))
+                                   .binding(defaults.musicDelayLength, () -> config.musicDelayLength, newVal -> config.musicDelayLength = newVal )
+                                   .controller(opt -> EnumControllerBuilder.create(opt).enumClass(MusicDelayLength.class))
+                                   .build())
+                           .option(Option.<Integer>createBuilder()
+                                   .name(Text.literal("Fadeout Speed"))
+                                   .description(OptionDescription.of(Text.literal("Higher Value means faster Fadeout. Max -> No Fading, Song will be cut off")))
+                                   .binding(defaults.standardFadeout, () -> config.standardFadeout, newVal -> config.standardFadeout = newVal)
+                                   .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                           .range(0,150)
+                                           .step(10))
+                                   .build()
+                           )
+                           .option(Option.<Integer>createBuilder()
+                                   .name(Text.literal("Event detection Speed. Higher Values means it checks more frequent. Max -> Instant Song Event Change, not recommended!!"))
+                                   .description(OptionDescription.of(Text.literal("")))
+                                   .binding(defaults.waitforSwitch, () -> config.waitforSwitch, newVal -> config.waitforSwitch = newVal)
+                                   .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                           .range(0,150)
+                                           .step(10))
+                                   .build()
+                           )
+                   .build())
+                   //Category / Tab "Debug"
+                   .category(ConfigCategory.createBuilder()
+                           .name(Text.literal("Debug"))
+                           .tooltip(Text.literal("Any debug tools useful for songpack creators or developers"))
+                           .option(Option.<Boolean>createBuilder()
                                     .name(Text.literal("Debug Mode Enabled"))
                                     .binding(defaults.debugModeEnabled, () -> config.debugModeEnabled, newVal -> config.debugModeEnabled = newVal )
                                     .controller(TickBoxControllerBuilder::create)
                                     .build())
-
-
-
-
-                            .build())
-
-
-
-
+                           .build())
             .build();
-
-
-
-
-
             return builder;
 
         })).generateScreen(parent);
@@ -196,7 +184,6 @@ public class ModConfig {
         GSON.save();
 
         SongLoader.setActiveSongpack(zip, embeddedMode);
-
 
     }
 
